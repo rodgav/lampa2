3e5c4d49b8072cb4eb920ae9a4ea7ce30623af33
RSGM <bankenro.myhope@gmail.com>
2021-04-22 22:14:02 -0500

    .....

---
+++ plugin/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=C:\src\flutter_stable"
+export "FLUTTER_APPLICATION_PATH=C:\Users\banke\AndroidStudioProjects\lampa2\plugin"
+export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build\ios"
+export "FLUTTER_BUILD_NAME=9.7.0"
+export "FLUTTER_BUILD_NUMBER=9.7.0"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=false"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.packages"
